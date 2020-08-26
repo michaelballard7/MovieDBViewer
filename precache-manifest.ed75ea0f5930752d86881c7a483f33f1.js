6f8aae6b660ef71dbcbc2ae2118b37d940ec9c73
Michael Ballard <bookstore707@gmail.com>
2020-08-25 21:11:21 -0400

    Updates

---
+++ precache-manifest.ed75ea0f5930752d86881c7a483f33f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f506d3357eea2716d125fa647fc40337",
+    "url": "/MovieDBViewer/index.html"
+  },
+  {
+    "revision": "7535b2a0e91545b547de",
+    "url": "/MovieDBViewer/static/js/2.f479fbe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieDBViewer/static/js/2.f479fbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958367091151b393146e",
+    "url": "/MovieDBViewer/static/js/main.d4945395.chunk.js"
+  },
+  {
+    "revision": "e2d2bd99d3ea8224b87f",
+    "url": "/MovieDBViewer/static/js/runtime-main.dab93ead.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/MovieDBViewer/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/MovieDBViewer/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/MovieDBViewer/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
