1a20c08b225ba6bd150f6cbe2138cf319b244c87
Michael Ballard <bookstore707@gmail.com>
2020-08-28 13:45:58 -0400

    complete author section

--- src/components/elements/Actor.js
+++ src/components/elements/Actor.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import NoImage from '../images/no_image.jpg';
+import { IMAGE_BASE_URL, POSTER_SIZE} from '../../config';
+import {StyledActor} from '../styles/StyledActor';
 
-function Actor() {
+function Actor({actor}) {
   return (
-    <div>
-      Actor
-    </div>
+    <StyledActor>
+      <img src={
+        actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
+      } alt="actorthumb"
+      />
+      <span className="actor-name">{actor.name}</span>
+      <span className="actor-character">{actor.character}</span>
+    </StyledActor>
   )
 }
 
--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { calcTime, convertMoney } from '../../helpers';
+import { StyledMovieInfoBar } from '../styles/StyledMovieInfoBar';
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+
+  <StyledMovieInfoBar>
+    <div className="movieinforbar-content">
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-time" name="clock-0" size="2x" />
+        <span className="moviebar-ino">
+          Running time: {calcTime(time)}
+        </span>
+      </div>
 
 
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-budget" name="money" size="2x" />
+        <span className="moviebar-ino">
+          Budget: {convertMoney(budget)}
+        </span>
+      </div>
 
-function MovieInfoBar({}) {
-  return (
-    <div></div>
-  )
-}
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-revenue" name="ticket" size="2x" />
+        <span className="moviebar-ino">
+          Revenue: {convertMoney(revenue)}
+        </span>
+      </div>
+
+
+    </div>
+  </StyledMovieInfoBar>
+)
 
 export default MovieInfoBar
